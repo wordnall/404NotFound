1192d8536724ce06b83f193cda697f3b82b1fe72
student <student@iMac-29.local>
2018-11-01 20:28:51 -0700

    created on click listeners for homepage buttons and renamed the buttons. Removed extra search button. Fixed syntax for text size in textview in main activity layout.

--- app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button searchActivity;
+    private Button searchActivity, randomSearchActivity, ingredientsActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
 
                 startActivity(intent);
+            }
+        });
+
+        randomSearchActivity = findViewById(R.id.random_search_button);
+        searchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, randomSearchActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        ingredientsActivity = findViewById(R.id.ingredients_page_button);
+        ingredientsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, randomSearchActivity.class));
             }
         });
     }
