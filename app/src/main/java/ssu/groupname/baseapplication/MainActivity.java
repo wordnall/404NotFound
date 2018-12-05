0ac401b74d5dc0e65eb472063a457ce158f1da8c
analogHyperdrive <caribblacktip@yahoo.com>
2018-12-05 22:44:13 +0000

    Fixing MainActivity.java

--- app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
@@ -8,11 +8,25 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button searchActivity;
+    private Button listButton, searchActivity, randomSearchActivity, ingredientsActivity;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        listButton =  findViewById(R.id.ingredients_page_button);// links to the ingreadents page/list page
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //do something
+                Intent intent = new Intent(MainActivity.this, listActivity.class);
+
+                startActivity(intent);
+            }
+        });
 
         searchActivity = findViewById(R.id.search_page_button);
         searchActivity.setOnClickListener(new View.OnClickListener() {
