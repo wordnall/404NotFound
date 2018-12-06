f3e230f9001bdfd1f65ca74eca10ceb7a7736e59
analogHyperdrive <caribblacktip@yahoo.com>
2018-12-06 23:07:45 +0000

    Added randomSearch

--- app/src/main/java/ssu/groupname/baseapplication/DBHandler.java
+++ app/src/main/java/ssu/groupname/baseapplication/DBHandler.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+
 
 public class DBHandler extends SQLiteOpenHelper {
 
@@ -67,9 +69,15 @@ public class DBHandler extends SQLiteOpenHelper {
         return ingredient;
     }
 
-    public String[] allNames() {
-        String query = "Select * FROM " + TABLE_INGREDIENTS + "'";
-        return null;
+    public ArrayList<String> allNames() {
+        String query = "Select * FROM " + TABLE_INGREDIENTS;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<String> result = new ArrayList<String>();
+        while (cursor.moveToNext()) {
+            result.add(cursor.getString(1));
+        }
+        return result;
     }
 
     public String loadHandler() {
--- app/src/main/java/ssu/groupname/baseapplication/RandomSearchActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/RandomSearchActivity.java
@@ -1,14 +1,43 @@
 package ssu.groupname.baseapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import ssu.groupname.Models.RecipeModel;
+import ssu.groupname.network.RecipeSearchAsyncTask;
+
 public class RandomSearchActivity extends AppCompatActivity {
 
-    DBHandler dbHandler = new DBHandler(this, null, null, 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DBHandler dbHandler = new DBHandler(this, null, null, 1);
+        ArrayList<String> dbIngredients = dbHandler.allNames();
+        Random rand = new Random();
+        String searchParam;
+        if (dbIngredients == null)
+            searchParam = "Apple";
+        else if (dbIngredients.size() == 2)
+            searchParam = dbIngredients.get(0);
+        else
+            searchParam = dbIngredients.get(rand.nextInt(dbIngredients.size()));
+        RecipeSearchAsyncTask task = new RecipeSearchAsyncTask();
+        task.setListener(new RecipeSearchAsyncTask.RecipeCallbackListener() {
+            @Override
+            public void onRecipeCallback(List<RecipeModel> models) {
+
+                Intent recyclerViewIntent = new Intent(RandomSearchActivity.this, SearchDetailActivity.class);
+
+                recyclerViewIntent.putExtra("details", (RecipeModel)models.get(0));
+                startActivity(recyclerViewIntent);
+            }
+        });
+        task.execute(searchParam);
     }
 }
